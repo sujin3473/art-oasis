f89e8e5199e502b6420fdd4b32c26b65d2ea90ba
sujin3473 <sujin3473@gmail.com>
2024-08-13 22:15:53 +0900

    add: pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import NavBar from "@/components/navBar";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-10">
-      <NavBar />
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between px-10 py-5">
+      메인 페이지
+    </div>
   );
 }
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => (
-  <div className="flex h-20 justify-between py-3 items-center">
+  <div className="flex h-20 w-full justify-between py-3 px-[10%] items-center shadow">
     <div className="flex items-center">
       <div className="w-[50px] h-[50px] relative items-center">
         <Image src="/images/aro_logo.png" alt="logo" fill />
       </div>
       <div className="font-bold text-2xl">ARO</div>
     </div>
-    <div className="flex"></div>
+    <div className="flex gap-x-4">
+      <LinkButton label="회사소개" link="/company" />
+      <LinkButton label="사업소개" link="/business" />
+      <LinkButton label="홍보·고객센터" link="/support" />
+      <LinkButton label="협력업체" link="/partners" />
+    </div>
   </div>
+);
+
+const LinkButton = ({ label, link }: { label: string; link: string }) => (
+  <Link href={link} className="px-8">
+    {label}
+  </Link>
 );
 
 export default NavBar;
